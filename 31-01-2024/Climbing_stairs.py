fb3d8ea5a1079a1256e8d89ade1660485095154d
sathpriya123 <114803974+sathpriya123@users.noreply.github.com>
2024-02-01 22:12:28 +0530

    Create Climbing_stairs.py

---
+++ 31-01-2024/Climbing_stairs.py
@@ -0,0 +1,58 @@
+"""
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+ 
+
+Constraints:
+
+1 <= n <= 45
+"""
+#logic 1 which is a Fibonacci sequence 
+ def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        one,two=1,1
+        
+        for i in range(n-1): 
+            temp=one
+            one=one+two
+            two=temp
+        return one
+
+#logic 2 dynamic programming approach 
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n==1: return 1
+        arr = [0]*n
+        arr[0] = 1
+        arr[1] = 2
+        
+        for i in range(2,n):
+            arr[i] = arr[i-1] + arr[i-2]
+        return arr[n-1]
+        
